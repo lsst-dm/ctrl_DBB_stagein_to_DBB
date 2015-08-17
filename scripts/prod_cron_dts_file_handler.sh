ac62212fa69db0ef4e549abe432ae12b1335fd31
Michelle Gower <mgower@illinois.edu>
2015-08-17 18:14:01 +0000

    Configuration changes made for running on desar3
    git-svn-id: https://dessvn.cosmology.illinois.edu/svn/desdm/devel/dtsfilereceiver/branches/wrapper-refact@39617 2422e80f-9519-0410-a819-8e21e7c86b6d

--- scripts/prod_cron_dts_file_handler.sh
+++ scripts/prod_cron_dts_file_handler.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-BASELOGDIR=/home/databot2/logs/dts_file_handler_logs
+BASELOGDIR=/local/dts_desdm/logs/dts_file_handler_logs
+SVNDIR=/local/dts_desdm/svncheckout
 
 themonth=`/bin/date +%Y/%m`
 logdir=$BASELOGDIR/$themonth
@@ -16,13 +17,14 @@ unset EUPS_DIR
 unset EUPS_PATH
 
 echo `/bin/date` " - " $$ " eups setup main stack " >> $logfile 2>&1
+#source /work/apps/RHEL6/dist/eups/desdm_eups_setup.sh 
 source /work/apps/RHEL6/dist/eups/desdm_eups_setup.sh 
 setup --nolocks Y2Nstack 1.0.6+0 >> $logfile 2>&1
 
 # dtsfilereceiver checkout from svn
 echo `/bin/date` " - " $$ " eups setup dtsfilereceiver " >> $logfile 2>&1
 cd /home/databot2/dtsfilereceiver/trunk
-setup --nolocks -r . >> $logfile 2>&1
+setup --nolocks -j -r . >> $logfile 2>&1
 
 #echo $DTSFILERECEIVER_DIR
 echo `/bin/date` " - " $$ " running dts_file_handler.py " >> $logfile 2>&1
